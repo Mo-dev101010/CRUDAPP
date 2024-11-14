39285d241b6c76a9ad1430a019e33ce4939d0869
Mo Khan <mo.khan101010@yahoo.com>
2024-11-14 10:53:26 +0000

    Add project files.

---
+++ Pages/Customers/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CRUDAPP.Data;
+using CRUDAPP.Model;
+
+namespace CRUDAPP.Pages.Customers
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly CRUDAPP.Data.CRUDAPPContext _context;
+
+        public DetailsModel(CRUDAPP.Data.CRUDAPPContext context)
+        {
+            _context = context;
+        }
+
+        public Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customer.FirstOrDefaultAsync(m => m.ID == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Customer = customer;
+            }
+            return Page();
+        }
+    }
+}
